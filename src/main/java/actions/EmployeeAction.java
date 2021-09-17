3a483e59f88353ae6631433c7763f175c9502925
yuuto.ooki <earth.423.eco@gmail.com>
2021-09-18 00:22:47 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -45,5 +45,13 @@ public class EmployeeAction extends ActionBase {
         
         forward(ForwardConst.FW_EMP_INDEX);
     }
+    
+    public void entryNew() throws ServletException, IOException{
+        
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView());
+        
+        forward(ForwardConst.FW_EMP_NEW);
+    }
 
 }
